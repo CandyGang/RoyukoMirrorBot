ead0adf1d2dae52c0b738d76a96768ad004a81f7
Shubhansh <74962930+Aruoto@users.noreply.github.com>
2021-07-16 18:54:15 +0530

    Initial commit

--- bot/__main__.py
+++ bot/__main__.py
@@ -52,16 +52,17 @@ def start(update, context):
     start_string = f'''
 This bot can mirror all your links to Google Drive!
 Type /{BotCommands.HelpCommand} to get a list of available commands
+Join: @SergeMirror For Support!
 '''
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/breakdowns/slam-mirrorbot")
-    buttons.buildbutton("Support Group", "https://t.me/SlamMirrorSupport")
+    buttons.buildbutton("Repo", "https://github.com/AniSerge/RoyukoMirrorBot")
+    buttons.buildbutton("Network", "https://t.me/AniSerge")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     LOGGER.info('UID: {} - UN: {} - MSG: {}'.format(update.message.chat.id, update.message.chat.username, update.message.text))
     uptime = get_readable_time((time.time() - botStartTime))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         if update.message.chat.type == "private" :
-            sendMessage(f"Hey I'm Alive 🙂\nSince: <code>{uptime}</code>", context.bot, update)
+            sendMessage(f"Hey I'm Alive\nSince: <code>{uptime}</code>", context.bot, update)
         else :
             update.effective_message.reply_photo(IMAGE_URL, start_string, parse_mode=ParseMode.MARKDOWN, reply_markup=reply_markup)
     else :
@@ -87,7 +88,6 @@ def ping(update, context):
 
 def log(update, context):
     sendLogFile(context.bot, update)
-
 
 def bot_help(update, context):
     help_string_adm = f'''
